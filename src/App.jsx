326f2afd09a3b6a155c196b9ee8235b7799dd0a1
EllieKaun <ilyafazylbaev@mail.ru>
2024-06-07 10:24:51 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MainPage from './pages/MainPage'
+import Header from './components/Header'
+import Container from './components/Container'
+// import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>  
+      <Container>
+        <Header />
+      </Container>
+      <MainPage />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
